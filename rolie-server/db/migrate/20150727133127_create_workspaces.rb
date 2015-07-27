089e92103f2c2fe7b30c065bffd829a3803f9021
KIKUCHI Masashi <kikuchi@lepidum.co.jp>
2015-07-27 22:33:42 +0900

    rails g model workspace title:string:uniq

---
+++ rolie-server/db/migrate/20150727133127_create_workspaces.rb
@@ -0,0 +1,10 @@
+class CreateWorkspaces < ActiveRecord::Migration
+  def change
+    create_table :workspaces do |t|
+      t.string :title
+
+      t.timestamps null: false
+    end
+    add_index :workspaces, :title, unique: true
+  end
+end

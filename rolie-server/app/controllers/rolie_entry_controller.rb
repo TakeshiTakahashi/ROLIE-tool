49f0f362b2771dbd9cf8f7e7ad99de8cdaac6459
KIKUCHI Masashi <kikuchi@lepidum.co.jp>
2015-07-31 20:54:22 +0900

    more action stub

--- rolie-server/app/controllers/rolie_entry_controller.rb
+++ rolie-server/app/controllers/rolie_entry_controller.rb
@@ -1,10 +1,33 @@
 class RolieEntryController < ApplicationController
   def index
+    workspace = Workspace.find_by!(path: params[:workspace])
+    collection = workspace.collections.find_by!(path: params[:collection])
+
     builder = Nokogiri::XML::Builder.new do |xml|
       xml.feed('xmlns' => 'http://www.w3.org/2005/Atom',
                'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
                'xsi:schemaLocation' => 'http://www.w3.org/2005/Atom file:/C:/schemas/atom.xsd urn:ietf:params:xml:ns:iodef-1.0 file:/C:/schemas/iodef-1.0.xsd'
                ) {
+        xml.generator "ROLIE prototype server"
+        xml.id_ "http://www.example.org/ TODO: stub"
+        xml.title('type' => 'text') {
+          xml.text collection.title
+        }
+        xml.updated # TODO
+        xml.author # TODO
+        xml.link
+
+        collection.entries.find_each {|entry|
+          xml.entry {
+            xml.id_ "TODO"
+            xml.title "TODO"
+            xml.link
+            xml.published "TODO"
+            xml.updated "TODO"
+            xml.category
+            xml.summary "TODO"
+          }
+        }
       }
     end
     render :xml => builder.to_xml(encoding: 'utf-8')

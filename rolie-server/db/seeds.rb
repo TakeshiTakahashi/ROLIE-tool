8cb75b210b2f53e5281a689a76b97890e06d5649
KIKUCHI Masashi <kikuchi@lepidum.co.jp>
2015-07-31 19:35:36 +0900

    seed sample

--- rolie-server/db/seeds.rb
+++ rolie-server/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+ws1 = Workspace.create(title: 'public', path: 'public')
+ws2 = Workspace.create(title: 'private', path: 'private')
+
+c1 = Collection.create(title: 'public incidents', path: 'incidents', workspace: ws1)
+c2 = Collection.create(title: 'private incidents', path: 'incidents', workspace: ws2)
+
+e1 = Entry.create(collection: c1, content: <<-XML)
+<?xml version="1.0" encoding="UTF-8"?>
+<iodef:IODEF-Document lang="en" xmlns:iodef="urn:ietf:params:xml:ns:iodef-1.0">
+</iodef:IODEF-Document>
+XML
